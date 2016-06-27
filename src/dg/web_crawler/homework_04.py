c8736e1c032d56c881f8742e5232c23bf08e1bfc
XenRon <xenron@outlook.com>
2016-06-27 04:39:15 +0000

    msg

--- src/dg/web_crawler/homework_04.py
+++ src/dg/web_crawler/homework_04.py
@@ -1,5 +1,3 @@
-from urllib2 import urlopen
-from bs4 import BeautifulSoup
 import re
 
 def validateEmail(email):
@@ -9,26 +7,34 @@ def validateEmail(email):
 
 
 def validateURL(url):
-    if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", url) != None:
+    if re.match(r'^https?:/{2}\w.+$', url) != None:
         return True
     return False
 
 
-def validateHtmlTag(tag, target):
-    if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", tag) != None:
+def validateHtmlImg(tag):
+    if re.compile(r"""<img\s.*?\s?src\s*=\s*['|"]?([^\s'"]+).*?>""",re.I) != None:
         return True
     return False
 
 
 def validateInteger(integer):
-    if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", integer) != None:
+    if re.match("^\+?[0-9]+$", integer) != None:
         return True
     return False
 
 
 if __name__ == '__main__':
+    print("validateEmail")
     print(validateEmail("abc@test.com"))
     print(validateEmail("abc#test.com"))
-    print(validateURL("abc#test.com"))
-    print(validateHtmlTag("abc#test.com", "<img>"))
+    print("validateURL")
+    print(validateURL("http://a.test.com/"))
+    print(validateURL("a.test.com"))
+    print("validateHtmlTag")
+    print(validateHtmlImg('<img src="icon.png">'))
+    print(validateHtmlImg('<input value="icon.png" />'))
+    print("validateInteger")
     print(validateInteger("1"))
+    print(validateInteger("+1"))
+    print(validateInteger("-1"))
