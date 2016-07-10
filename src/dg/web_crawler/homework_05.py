24681721c1a6661b92aff764fcd6a075608c2da2
XenRon <xenron@outlook.com>
2016-07-10 12:51:23 +0000

    msg

---
+++ src/dg/web_crawler/homework_05.py
@@ -0,0 +1,18 @@
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html,'lxml')
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print(newPage)
+
+
+if __name__ == '__main__':
+    getLinks("")
