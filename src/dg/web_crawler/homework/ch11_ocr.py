dbeee5d55507c8bfc6058ba2e9579d14d4bad084
xenron <xenron@hotmail.com>
2016-08-22 00:56:35 +0900

    msg

---
+++ src/dg/web_crawler/homework/ch11_ocr.py
@@ -0,0 +1,5 @@
+# TESSDATA_PREFIX
+# C:\Program Files (x86)\Tesseract-OCR\
+
+# set OCR_IMAGE_PATH=D:\tmp\github\my\sandbox-da-python\src\dg\web_crawler\data
+# tesseract.exe %OCR_IMAGE_PATH%\1356422284_1112.jpg result -l eng
