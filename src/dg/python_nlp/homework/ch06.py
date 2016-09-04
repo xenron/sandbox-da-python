1991212fe6cc6dec662f452b3e07a90dbacd107a
xenron <xenron@hotmail.com>
2016-09-04 19:59:29 +0900

    msg

--- src/dg/python_nlp/homework/ch06.py
+++ src/dg/python_nlp/homework/ch06.py
@@ -11,8 +11,10 @@ import jieba.analyse
 from nltk.corpus import brown
 import nltk
 
+# 10. ○ Train a unigram tagger and run it on some new text. Observe that some words are not assigned a tag. Why not?
 def q10():
     fd = nltk.FreqDist(brown.words(categories='news'))
+    # brown_tagged_sents = nltk.FreqDist(brown.words(categories='news'))
     cfd = nltk.ConditionalFreqDist(brown.tagged_words(categories='news'))
     most_freq_words = fd.most_common(100)
     likely_tags = dict((word, cfd[word].max()) for (word, __) in most_freq_words)
