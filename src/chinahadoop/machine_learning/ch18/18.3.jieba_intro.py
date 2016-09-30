70f5515700d43e363b8408217f822cbd7f5bc739
xenron <xenron@hotmail.com>
2016-10-01 05:53:56 +0900

    msg

---
+++ src/chinahadoop/machine_learning/ch18/18.3.jieba_intro.py
@@ -0,0 +1,18 @@
+# -coding:utf-8-
+
+import sys
+import jieba
+import jieba.posseg
+
+
+if __name__ == "__main__":
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    f = open('.\\text\\18.novel.txt')
+    str = f.read().decode('utf-8')
+    f.close()
+
+    seg = jieba.posseg.cut(str)
+    for s in seg:
+        # print s.word, s.flag,
+        print s.word, '|',
