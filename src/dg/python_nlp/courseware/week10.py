fabe29a481273a0b08edf45f4417945d0bec7fe0
xenron <xenron@hotmail.com>
2016-10-15 20:27:54 +0900

    msg

--- src/dg/python_nlp/courseware/week10.py
+++ src/dg/python_nlp/courseware/week10.py
@@ -4,13 +4,18 @@ import nltk
 
 
 #文法
+# S，句子开始
+# NP，名词短语
+# VP，动词短语
+# V，动词
+# AP，形容词
 grammar = nltk. CFG.fromstring("""
 S -> NP VP
 VP -> V AP | V NP
 V -> '是'|'走在'|'进入'
 AP -> '很抽象的'
 NP -> '北京'|'哈尔滨'|'形式语言'|'中国'|'教育'|'集合'|'WTO'|'美丽的城市'|'祖国的首都'|'数学的基础'|'社会发展的前面'
-""",encoding='utf-8')   
+""",encoding='utf-8')
 
 sent = [u'集合',u'是',u'数学的基础']
 parser = nltk.ChartParser(grammar)
@@ -20,7 +25,8 @@ for tree in trees:
     tree.draw()
 
 
-#普遍存在的歧义
+# 普遍存在的歧义
+# Det，限定词
 groucho_grammar = nltk.CFG.fromstring("""
 S -> NP VP
 NP -> Det NP | Det AP N | AP N  | N
@@ -29,14 +35,14 @@ N -> '校长'|'座谈会'
 Det -> '三个'
 AP -> '学校的'
 V -> '参加'
-""",encoding='utf-8')    
-    
+""",encoding='utf-8')
+
 sent = [u'三个', u'学校的', u'校长', u'参加', u'座谈会']
 parser = nltk.ChartParser(groucho_grammar)
 trees = parser.parse(sent)
 for tree in trees:
-        tree.draw() 
-    
+        tree.draw()
+
 #上下文无关文法
 grammar1 = nltk.CFG.fromstring("""
 S -> NP VP
@@ -52,14 +58,13 @@ sent = "Mary saw Bob".split()
 rd_parser = nltk.RecursiveDescentParser(grammar1)
 for tree in rd_parser.parse(sent):
     print tree
-    
+
 sent="the dog saw a man in the park".split()
 rd_parser = nltk.RecursiveDescentParser(grammar1)
 for tree in rd_parser.parse(sent):
-    tree.draw()    
-    
-    
-    
+    tree.draw()
+
+
 #递归的上下文无关文法
 grammar2 = nltk.CFG.fromstring("""
 S -> NP VP
@@ -165,17 +170,17 @@ for tree in trees:
 #文法开发
 from nltk.corpus import treebank
 t = treebank.parsed_sents('wsj_0001.mrg')[0]
-print t  
+print t
 
 def filter(tree):
     child_nodes = [child.label() for child in tree
                    if isinstance(child, nltk.Tree)]
     return  (tree.label() == 'VP') and ('S' in child_nodes)
-    
-from nltk.corpus import treebank  
+
+from nltk.corpus import treebank
 [subtree for tree in treebank.parsed_sents()
          for subtree in tree.subtrees(filter)]
-             
+
 from collections import defaultdict
 entries = nltk.corpus.ppattach.attachments('training')
 table = defaultdict(lambda: defaultdict(set))
@@ -190,7 +195,7 @@ for key in sorted(table):
 nltk.corpus.sinica_treebank.parsed_sents()[3450].draw()
 
 
- 	
+
 grammar = nltk.CFG.fromstring("""
 S -> NP V NP
 NP -> NP Sbar
@@ -208,33 +213,33 @@ tokens = ["fish"] * 7
 cp = nltk.ChartParser(grammar)
 for tree in cp.parse(tokens):
     print(tree)
-    
+
 tokens = ["fish"] * 9
 cp = nltk.ChartParser(grammar)
 for tree in cp.parse(tokens):
     print(tree)    
-    
+
 #加权文法
 def give(t):
     return t.label() == 'VP' and len(t) > 2 and t[1].label() == 'NP'\
            and (t[2].label() == 'PP-DTV' or t[2].label() == 'NP')\
            and ('give' in t[0].leaves() or 'gave' in t[0].leaves())
-           
+
 def sent(t):
     return ' '.join(token for token in t.leaves() if token[0] not in '*-0')
-    
+
 def print_node(t, width):
         output = "%s %s: %s / %s: %s" %\
             (sent(t[0]), t[1].label(), sent(t[1]), t[2].label(), sent(t[2]))
         if len(output) > width:
             output = output[:width] + "..."
         print(output)    
-        
+
 for tree in nltk.corpus.treebank.parsed_sents():
     for t in tree.subtrees(give):
        print_node(t, 72)    
-       
-       
+
+
 #概率上下文无关文法
 grammar = nltk.PCFG.fromstring("""
     S    -> NP VP              [1.0]
