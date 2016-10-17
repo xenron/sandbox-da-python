201d8c1bf6c9c92427c971e571fb1c1df2612f66
XenRon <xenron@outlook.com>
2016-10-17 12:27:25 +0000

    msg

---
+++ src/dg/python_nlp/homework/ch12.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# 1. 《Python自然语言分析》第11章课后习题：1,4
+
+# 1. In Example 11-2 the new field appeared at the bottom of the entry. Modify this
+# program so that it inserts the new subelement right after the lx field. (Hint: create
+# the new cv field using Element('cv'), assign a text value to it, then use the
+# insert() method of the parent element.)
+
+# 在例11-2 中新字段出现在条目底部。修改这个程序使它就在lx 字段后面插入新的子
+# 元素。（提示：使用Element('cv')创建新的cv 字段，分配给它一个文本值，然后使用
+# 父元素的insert()方法。）
+
+
+
+
+# 4. Write a program to find any parts-of-speech (ps field) that occurred less than 10
+# times. Perhaps these are typing mistakes?
+
+# 写一个程序，找出所有出现少于10 次的词性（ps 字段）。或许有打字错误？
+
+
+
